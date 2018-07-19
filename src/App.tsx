0767a3d9754968715173f4255318a11a73654317
Philip Fulgham <philfulgham@gmail.com>
2018-07-18 22:02:36 -0400

    Scaffold ContactInfo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import "./App.css";
 
-import logo from "./logo.svg";
+import ContactInfo from "./components/ContactInfo/ContactInfo";
 
 import details from "./details.json";
 
@@ -9,13 +9,7 @@ class App extends React.Component {
 	public render() {
 		return (
 			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.tsx</code> and save to reload.
-				</p>
+				<ContactInfo data={details} />
 			</div>
 		);
 	}
---
+++ src/components/ContactInfo/ContactInfo.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import "./ContactInfo.css";
+
+interface IProps {
+	data: any;
+}
+
+export default class ContactInfo extends React.Component<IProps> {
+	public render() {
+		return (
+			<div className="ContactInfo">
+				<div className="ContactInfo-name">
+					{this.props.data.name}
+				</div>
+				<div className="ContactInfo-bio">
+					{this.props.data.bio}
+				</div>
+				<div className="ContactInfo-address">
+					{this.props.data.address.street}<br />
+					{this.props.data.address.city}, {this.props.data.address.state} {this.props.data.address.zipCode}
+				</div>
+				<div className="ContactInfo-email">
+					{this.props.data.email}
+				</div>
+			</div>
+		);
+	}
+}
